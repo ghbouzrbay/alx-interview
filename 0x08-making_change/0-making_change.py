26d822c10c5a4964c9aabc1415d669532483717c
ghbouzrbay <atighmarcelline@gmail.com>
2024-02-29 23:23:05 +0100

    done

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,12 +9,24 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    min_coins = total + 1
-    for coin in coins:
-        if coin <= total:
-            num_coins = 1 + makeChange(coins, total - coin)
-            if num_coins < min_coins:
-                min_coins = num_coins
-    if min_coins > total:
-        return -1
-    return min_coins
+    if total in memo:
+        return memo[total]
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(sorted_coins)
+    while rem > 0:
+        if coin_idx >= n:
+            memo[total] = -1
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            num_coins = rem // sorted_coins[coin_idx]
+            rem %= sorted_coins[coin_idx]
+            coins_count += num_coins
+        else:
+            coin_idx += 1
+    memo[total] = coins_count
+    return coins_count
+
+memo = {}
